0423f597039d4d3bc4b9f58a9ea798576f83a2b7
Pascal Andermatt <p.andermatt@me.com>
2022-02-24 19:18:46 +0100

    add exporting

--- algo/sample_algorithm.rb
+++ algo/sample_algorithm.rb
@@ -4,6 +4,10 @@ class SampleAlgorithm
   def call
     numbers = context.world&.numbers
 
+    b = Person.new(name: 'Bob')
+    a = Person.new(name: 'Anna')
+    context.project_assignments = [ProjectAssignment.new(name: 'WebServer', people: [b, a])]
+
     if numbers
       context.sum = numbers.reduce(&:+)
     else
--- lib/google_exporting.rb
+++ lib/google_exporting.rb
@@ -1,15 +1,11 @@
 module GoogleExporting
   def self.write_result(result, output_path)
     File.open(output_path, 'w') do |file|
-      file.puts result.sum
-      # file.puts valid_intersections.length
-      # valid_intersections.each do |intersection|
-      #   file.puts intersection.id
-      #   file.puts intersection.schedule_entries.length
-      #   intersection.schedule_entries.each do |entry|
-      #     file.puts "#{entry.street_name} #{entry.duration}"
-      #   end
-      # end
+      file.puts result.project_assignments.length
+      result.project_assignments.each do |assignment|
+        file.puts assignment.name
+        file.puts assignment.people.map(&:name).join(" ")
+      end
     end
   end
 end
