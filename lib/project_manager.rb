f451096ced13e620c9177cd5bb7b66c4be229007
Josua Schmid <josua.schmid@renuo.ch>
2022-02-24 21:25:44 +0100

    Add levelup

--- lib/project_manager.rb
+++ lib/project_manager.rb
@@ -13,5 +13,18 @@ ProjectManager = Struct.new(:project_assignments, :current_day, keyword_init: tr
       c.used = false
     end
     assignment.project.done = true
+
+    # level up if skills have been used at the edge
+    assignment.project.skills.each do |ps|
+      # edge
+      good_skill = assignment.contributors.flat_map(&:skills).find { |cs| ps.name == cs.name && ps.level == cs.level }
+      good_skill.level += 1 if good_skill
+
+      # mentored
+      if good_skill
+        skill = assignment.contributors.flat_map(&:skills).find { |cs| ps.name == cs.name && ps.level == (cs.level + 1) }
+        skill.level += 1 if skill
+      end
+    end
   end
 end
