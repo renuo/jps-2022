c6fe95d9bc3508acfa956572ee90a9d442535629
Josua Schmid <josua.schmid@renuo.ch>
2022-02-24 19:29:11 +0100

    Add imprting

--- lib/google_importing.rb
+++ lib/google_importing.rb
@@ -5,38 +5,42 @@ module GoogleImporting
 
   def self.build_world(dataset_name)
     lines = File.read("data/in/#{dataset_name}.txt").split(/\n/)
-    # first_line = lines[0].split
-    # simulation_time = first_line[0].to_i
-    # intersections_count = first_line[1].to_i
-    # streets_count = first_line[2].to_i
-    # cars_count = first_line[3].to_i
-    # bonus_points = first_line[4].to_i
-    # intersections = {}
-    # streets_hash = {}
-    # streets_count.times.map do |i|
-    #   street_line = lines[i + 1].split
-    #   start_intersection_id = street_line[0]
-    #   end_intersection_id = street_line[1]
-    #   street_name = street_line[2]
-    #   street_length = street_line[3]
-    #   street = Street.new(start: start_intersection_id, end: end_intersection_id, name: street_name, length: street_length, cars: [])
-    #   streets_hash[street_name] = street
-    #   intersections[start_intersection_id] ||= Intersection.new(id: start_intersection_id, out_streets: [], in_streets: [], schedule_entries: [])
-    #   intersections[start_intersection_id].out_streets << street
-    #   intersections[end_intersection_id] ||= Intersection.new(id: end_intersection_id, out_streets: [], in_streets: [], schedule_entries: [])
-    #   intersections[end_intersection_id].in_streets << street
-    # end
-    # cars = cars_count.times.map do |i|
-    #   car_line = lines[i + 1 + streets_count].split
-    #   street_names = car_line[1..-1]
-    #   car = Car.new(streets: street_names.map { |street_name| streets_hash[street_name]})
-    #   street_names.each do |street_name|
-    #     streets_hash[street_name].cars << car
-    #   end
-    #   car
-    # end
 
-    numbers = lines[0].split(' ').map(&:to_i)
-    World.new(numbers: numbers)
+    contributor_count, project_count = lines[0].split
+    i = 1
+
+    contributors = []
+    contributor_count.to_i.times do |c|
+      contributor_name, contributor_skill_count = lines[i].split
+      contributor = Contributor.new(name: contributor_name)
+      i += 1
+
+      contributor_skill_count.to_i.times do |sc|
+        skill_name, skill_level = lines[i].split
+        contributor.skills << Skill.new(name: skill_name, level: skill_level)
+        i += 1
+      end
+
+      contributors << contributor
+    end
+
+    projects = []
+    project_count.to_i.times do |p|
+      project_name, duration_days, score, best_before, role_count = lines[i].split
+      project = Project.new(name: project_name, duration_days: duration_days, score: score)
+      i += 1
+
+      role_count.to_i.times do |rc|
+        skill_name, skill_level = lines[i].split
+        project.skills << Skill.new(name: skill_name, level: skill_level)
+        i += 1
+      end
+
+      projects << project
+    end
+
+    puts "at #{i} of #{lines.length}"
+
+    World.new(contributors: contributors, projects: projects)
   end
 end
--- lib/world.rb
+++ lib/world.rb
@@ -1,15 +1,31 @@
-Person = Struct.new(:name, keyword_init: true) do
+Skill = Struct.new(:name, :level, keyword_init: true) do
+  def initialize(args)
+    super(args)
+  end
 end
 
+Contributor = Struct.new(:name, :skills, keyword_init: true) do
+  def initialize(args)
+    @skills = []
+    super(args)
+  end
+end
+
+Project = Struct.new(:name, :duration_days, :score, keyword_init: true) do
+  def initialize(args)
+    @skills = []
+    super(args)
+  end
+end
+    
 ProjectAssignment = Struct.new(:name, :people, keyword_init: true) do
 end
 
-World = Struct.new(:numbers, keyword_init: true) do
+World = Struct.new(:contributors, :projects, keyword_init: true) do
   def initialize(args)
+    @contributors = []
+    @projects = []
     super(args)
-
-    # Manipulate world setup starting from here:
-    # …
   end
 end
 
