eba98dc8de6dd421c5445bbafb96894c561457c6
Pascal Andermatt <p.andermatt@me.com>
2022-02-24 19:37:46 +0100

    update exporting

--- lib/google_exporting.rb
+++ lib/google_exporting.rb
@@ -1,10 +1,11 @@
 module GoogleExporting
   def self.write_result(result, output_path)
     File.open(output_path, 'w') do |file|
-      file.puts result.project_assignments.length
-      result.project_assignments.each do |assignment|
+      file.puts result.project_manager.length
+      puts result.project_manager
+      result.project_manager.project_assignments.each do |assignment|
         file.puts assignment.name
-        file.puts assignment.people.map(&:name).join(" ")
+        file.puts assignment.contributors.map(&:name).join(" ")
       end
     end
   end
